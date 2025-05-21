58d3792093aecc787883c9de7093dc83188bc793
sagar sedhain <sagarsedhain09@gmail.com>
2025-05-21 17:11:09 +0200

    changes on variable

---
+++ day_1.py
@@ -0,0 +1,50 @@
+#variables
+x =3
+print(type(x))
+
+y = 4.6
+print(type(y))
+
+# Variable and operators
+
+#addition
+a=3
+b=6
+sum= a+b
+print(sum)
+
+#substraction
+a=3
+b=6
+sub= a-b
+print(sub)
+
+#division
+a=6
+b=3
+div= a/b
+print(div)
+
+#power
+a =4
+print(a**2)
+
+#Concatenation of string
+h1= "hello"
+h2 = "world"
+h = h1 + h2
+print(h)
+
+
+#Boolean Variable
+ 
+a = True
+b = False
+c = True
+d = not(b)
+print (type(a))
+print (type(b))
+print (d)
+print(a and b )
+print(a and c)
+print((a and b) or (b or d))
