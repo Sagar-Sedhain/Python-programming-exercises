2d220f125ab1239c4b4674218b69d18ec2f41e49
sagar sedhain <sagarsedhain09@gmail.com>
2025-05-22 21:12:48 +0200

    some function used in python

---
+++ day_2.py
@@ -0,0 +1,33 @@
+# Round function
+print (round(2.3))
+
+print (round(2.4432, 3)) #round up to 3 decimal
+
+#div mod function
+print(divmod(24,6)) #it gives quotent and remainder
+
+a = 34
+b = 10
+c = divmod(a,b)
+print(c)
+print (type(c))
+print(c[0])
+print(c[1])         #index
+
+#isinstance function -> multiple class check
+
+print(isinstance(3,int))
+print(isinstance(3,float))
+
+a = pow(3,2)
+print(a)
+
+# input ()
+#a = input ("enter number:")
+#print(type(a))
+#a = input ("enter number:")
+#b = int(a)
+#print(type(b))
+
+#a = float(input("enter a number:"))
+
