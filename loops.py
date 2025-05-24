fecc0745fc0be0d69b30dd5cc77e7d53151bddcd
sagar sedhain <sagarsedhain09@gmail.com>
2025-05-24 14:50:49 +0200

    for loops

--- loops.py
+++ loops.py
@@ -7,8 +7,8 @@ while i < r:
     print (i**2)
     print("This is iteration number :", i)
     i+=1            # i = i+1
-print("close loop") """
-
+print("close loop")
+ """
 
 i = 1
 while True:
@@ -21,7 +21,7 @@ while True:
 print("loops finished")
 
 
-""" i = 1
+i = 1
 while True:
     if i%9 != 0:
         print("inside IF")
@@ -31,4 +31,53 @@ while True:
         print("something")
         print("somethingElse")
         break
-print("loops finished") """
+print("loops finished")
+
+
+# For loop
+A = []
+#for i in range(10):
+for i in range(0,10,2):
+     
+    print(i)
+    A.append(i**2)
+print(A) 
+
+
+# else in for Loops
+S = {"apple",3.9,"cherry", "hello", "ram", 4}
+i = 1
+for x in S:
+    print(x)
+    i = i+1
+    if i ==4:
+        break
+    else:
+        pass 
+else:
+    print("Loops terminate")
+print("out from the loop")
+ 
+A ={"hello":11, "ram":12, "krishna":"abc"}
+for x in A:
+    print(x,A[x])  
+    
+    
+# print in increasing  order
+    
+A = [1,2,3,-5,7,9,3,2]
+for j in range (len(A)):
+    B = A[j]
+    idx = j
+    c = j
+    for i in range(j, len(A)):
+        if A[i]<B:
+            B = A[i]
+            idx = c
+        c += 1
+    tmp = A[j]
+    A[j] = B
+    A[idx] = tmp
+print(A)
+
+    
