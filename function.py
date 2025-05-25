348b4e6964b048a1b7358755078b82ea1228198f
sagar sedhain <sagarsedhain09@gmail.com>
2025-05-25 15:08:05 +0200

    implementing different function with input argument

---
+++ function.py
@@ -0,0 +1,33 @@
+# Defining of function
+def printSucess():
+    print("hello")
+    print("world")
+
+printSucess()
+
+
+def printSuccess1():
+    print("hello")    
+printSuccess1()
+
+
+# Function (input argument)
+def print_message(msg):
+    print(msg)
+    
+print_message(2)
+
+
+
+def printMsg(msg):
+    if isinstance(msg,int):    
+        print(msg)
+    else:
+        print("your input argument is not number")
+printMsg("hello")
+
+y = 12
+printMsg(y)
+
+# Multiple arguments
+
