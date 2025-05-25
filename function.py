9fb33c74f0a825e72003c7988fb8b642d3d68c6c
sagar sedhain <sagarsedhain09@gmail.com>
2025-05-25 23:23:12 +0200

    Python function concepts, including defining functions, handling input arguments

--- function.py
+++ function.py
@@ -18,7 +18,6 @@ def print_message(msg):
 print_message(2)
 
 
-
 def printMsg(msg):
     if isinstance(msg,int):    
         print(msg)
@@ -44,3 +43,103 @@ def checkArgs(a,b,c):
         print("error")
 checkArgs(1,2,3)
 checkArgs(2,1,"hell0")
+
+
+# Order of input function
+
+def f(a,b,c):
+    print("A is :", a)
+    print("B is :", b)
+    print("c is :", c)
+    
+f(2,3,"hello")
+f(c= "hello", b= 2, a = 1)
+
+
+# Function (variables inside)
+
+def addition(x,y):
+    sumValue = x+y
+    print(sumValue)
+    
+addition(7,4)
+
+
+# function (return statement)
+# Scope of variable
+
+def addition(x,y):
+    sumValue = x+y
+    return sumValue
+    
+sumValue = addition(7,4)
+
+print(sumValue)
+
+
+# Variable outside the function
+
+variableOutSidefunction = 3
+
+def x():
+    variableOutSidefunction = 5
+    #print(variableOutSidefunction)
+x()
+print(variableOutSidefunction)
+
+
+def a():
+    print("A")
+    q = 3
+    w = 5
+    c = q+w
+    print("B")
+    return
+    print("hello")  
+a()
+    
+    
+def r():
+    a = 1
+    b = 2
+    c = 3
+    d = "hello"
+    return a,b,c,d
+w,x,y,z = r()
+
+print (w,x,y,z)
+
+
+# Functions number of input arguments
+
+def add1(*numb):
+    sum = 0
+    for i in range(len(numb)):
+        sum += numb[i] # sum = sum + numb [i]
+    return sum
+print(add1(1,2,3,4,5,9.1))
+
+
+# function  variable number of input variable 
+
+def variable(**numb):
+    for x in numb:
+        print (x, numb[x])
+variable(a = 3, b = 2, c = "nuibiwe")
+
+
+
+# function default value
+
+A = [1,2,3]
+A2 = A
+A2[0] = 7
+print (A)
+
+def  AA(A = [0,2,3,4]):
+    for i in A:
+        print (i)
+A2 = [1,6,7]
+AA()
+AA(A2)
+        
